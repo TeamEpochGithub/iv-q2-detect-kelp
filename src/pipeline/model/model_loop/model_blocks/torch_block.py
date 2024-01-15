bad09d941024ea4998572f13c65ea619b1c26119
Jasper van Selm <j.m.vanselm@student.tudelft.nl>
2024-01-15 16:40:14 +0100

    Fix pre-commit

--- src/pipeline/model/model_loop/model_blocks/torch_block.py
+++ src/pipeline/model/model_loop/model_blocks/torch_block.py
@@ -10,7 +10,6 @@ import albumentations
 import dask.array as da
 import numpy as np
 import torch
-import wandb
 from annotated_types import Gt
 from joblib import hash
 from sklearn.base import BaseEstimator, TransformerMixin
@@ -21,6 +20,7 @@ from torch.optim.lr_scheduler import LRScheduler
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
+import wandb
 from src.logging_utils.logger import logger
 from src.logging_utils.section_separator import print_section_separator
 from src.pipeline.model.model_loop.model_blocks.utils.dask_dataset import Dask2TorchDataset
