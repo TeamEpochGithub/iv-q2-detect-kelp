80bf249bd67bd51979be8c27ba19c587375f6e98
hjdeheer <hugodeheer1234@gmail.com>
2024-01-24 14:33:22 +0100

    Add temp

---
+++ src/pipeline/model/feature/transformation/stretch.py
@@ -0,0 +1,68 @@
+"""Pipeline step to stretch features based on kelp ranges."""
+import sys
+import time
+from dataclasses import dataclass, field
+
+import dask.array as da
+import dask_image.ndfilters._gaussian as gaf
+import dask_image.ndfilters._utils as gau
+import numpy as np
+from sklearn.base import BaseEstimator, TransformerMixin
+from tqdm import tqdm
+
+from src.logging_utils.logger import logger
+
+if sys.version_info < (3, 11):
+    from typing_extensions import Self
+else:
+    from typing import Self
+
+
+@dataclass
+class Stretch(BaseEstimator, TransformerMixin):
+    """Pipeline step to stretch features based on kelp ranges.
+
+    """
+    feature_ranges: list[list[float, float]] = field(default_factory=list)
+
+    def fit(self, X: da.Array, y: da.Array | None = None) -> Self:  # noqa: ARG002
+        """Do nothing. Exists for Pipeline compatibility.
+
+        :param X: UNUSED data to fit.
+        :param y: UNUSED target variable.
+        :return: The fitted transformer.
+        """
+        return self
+
+    def transform(self, X: da.Array) -> da.Array:
+        """Transform the data.
+
+        :param X: The data to transform
+        :return: The transformed data
+        """
+        time_start = time.time()
+
+        X = X.astype(np.float32)
+        X = self.clip(X)
+
+        # Apply this function across the channels
+        logger.info(f"Stretching complete in: {time.time() - time_start} seconds.")
+
+        return X
+
+    def clip(self, X: da.Array) -> da.Array:
+        """Clip the values in the array according to the kelp ranges specified in the config.
+
+        :param X: The data to clamp
+        :return: The clamped data
+        """
+        # Apply this function across the channels
+        results = []
+        for c, band in tqdm(enumerate(self.feature_ranges)):
+            low = band[0]
+            high = band[1]
+            modified_channel = np.clip(X[:, c, :, :], low, high)
+            results.append(modified_channel)
+
+        return da.stack(results, axis=1) if len(results) > 1 else X
+
