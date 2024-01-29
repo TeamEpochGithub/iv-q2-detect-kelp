288f363a0b38d0d648932b87bd3840ebaf3ca4db
Jasper van Selm <j.m.vanselm@student.tudelft.nl>
2024-01-29 14:10:43 +0100

    Add auxiliary loss module and model architecture

---
+++ src/pipeline/model/architectures/auxiliary_model.py
@@ -0,0 +1,53 @@
+"""Padded model architecture, suitable for instantiation for pytorch built-in models."""
+import torch
+from torch import nn
+
+
+class AuxiliaryModel(nn.Module):
+    """Model architecture with padding.This class is used to wrap a pytorch model and add padding to the input image if necessary.
+
+    :param model: Pytorch model to be used
+    :param padding: padding to be applied to the input image (to allow, for example, Unet to work with 350x350 images)
+    :param activation: activation function to be applied to the output of the model
+    """
+
+    def __init__(self, model: nn.Module, padding: int = 0, activation: None | nn.Module = None) -> None:
+        """Initialize the PaddedModel.
+
+        :param model: Pytorch model to be used
+        :param padding: padding to be applied to the input image (to allow, for example, Unet to work with 350x350 images)
+        :param activation: activation function to be applied to the output of the model
+        """
+        super().__init__()
+        self.model = model
+        self.padding = padding
+        self.activation = activation
+        # Create a padding layer to pad the input image to a suitable size
+        if padding > 0:
+            self.padding_layer = nn.ZeroPad2d((padding, padding, padding, padding))
+
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Forward pass.
+
+        :param x: input tensor
+        :return: output tensor
+        """
+        # Pad the input image if necessary
+        if self.padding > 0:
+            x = self.padding_layer(x)
+
+        # Forward pass
+        y = self.model(x).squeeze(axis=1)
+
+        # Remove the padding if necessary
+        if self.padding > 0:
+            y = y[:,:, self.padding : -self.padding, self.padding : -self.padding]
+        if self.activation is not None:
+            y = self.activation(y)
+
+        softmax_result = self.softmax(y[:, 1:])
+        y = torch.cat((y[:, :1], softmax_result), dim=1)
+
+        return y
