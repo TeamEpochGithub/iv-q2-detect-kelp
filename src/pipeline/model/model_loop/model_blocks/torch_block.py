c66fb0c8ed06f0a229b37eab971d4263900e9d6e
Jasper van Selm <j.m.vanselm@student.tudelft.nl>
2024-02-01 10:48:10 +0100

    Update train.yaml and torch_block.py, delete auxiliary_model.py, add hugo1_aux.yaml

--- src/pipeline/model/model_loop/model_blocks/torch_block.py
+++ src/pipeline/model/model_loop/model_blocks/torch_block.py
@@ -383,7 +383,29 @@ class TorchBlock(BaseEstimator, TransformerMixin):
                 
                 # forward pass
                 y_pred = self.model(X_batch).cpu().numpy()
-                preds.extend(y_pred)
+
+                if y_pred.shape[1] == 1:
+                    preds.extend(y_pred)
+                elif y_pred.shape[1] == 2:
+                    y_pred = np.argmax(y_pred, axis=1)
+                    preds.extend(y_pred)
+                elif y_pred.shape[1] == 3:
+                    regression_preds = y_pred[:, 0] > 0.5
+                    classification_preds = y_pred[:, 1:].argmax(axis=1)
+
+                    stacked_preds = np.stack([regression_preds, classification_preds], axis=1)
+
+                    # Perform the logical OR operation on the two channels
+                    union_preds = np.logical_or(stacked_preds[:, 0], stacked_preds[:, 1])
+
+                    # Convert the boolean array to an integer array
+                    union_preds = union_preds.astype(np.uint8)
+
+                    # preds.extend(union_preds)
+                    preds.extend(union_preds)
+                else:
+                    raise ValueError(f"Invalid number of channels in the output of the model: {y_pred.shape[1]}")
+
         logger.info("Done predicting")
 
         return np.array(preds)
