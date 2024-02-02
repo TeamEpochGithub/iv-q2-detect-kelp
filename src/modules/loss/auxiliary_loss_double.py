f16722e164bc25073d82ae566540667af70bd2aa
Jasper van Selm <j.m.vanselm@student.tudelft.nl>
2024-02-02 09:11:44 +0100

    Update lyu.yaml and auxiliary_loss_double.py

--- src/modules/loss/auxiliary_loss_double.py
+++ src/modules/loss/auxiliary_loss_double.py
@@ -2,11 +2,11 @@
 from dataclasses import dataclass
 
 import torch
-from segmentation_models_pytorch.losses.dice import DiceLoss
-from segmentation_models_pytorch.losses.focal import FocalLoss
+from segmentation_models_pytorch.losses.focal import FocalLoss as FocalLossSMP
 from torch import nn
 
 from src.logging_utils.logger import logger
+from segmentation_models_pytorch.losses import DiceLoss as DiceLossSMP
 
 
 @dataclass
@@ -21,8 +21,8 @@ class AuxiliaryLossDouble(nn.Module):
     def __post_init__(self) -> None:
         """Initialize the AuxiliaryLoss."""
         super().__init__()
-        self.classification_loss_1 = DiceLoss(mode="multiclass")
-        self.classification_loss_2 = FocalLoss(mode="multiclass")
+        self.classification_loss_1 = DiceLossSMP(mode="multiclass")
+        self.classification_loss_2 = FocalLossSMP(mode="multiclass")
         self.regression_loss_1 = torch.nn.MSELoss()
         self.regression_loss_2 = torch.nn.L1Loss()
 
@@ -33,16 +33,12 @@ class AuxiliaryLossDouble(nn.Module):
         :param targets: target tensor
         :return: loss
         """
-        # Check inputs shape is correct, last channel should be size 3
-        if preds.shape[-3] != 3:
-            logger.warning(f"Second last channel should be size 3, got {preds.shape[-3]}")
-            raise ValueError(f"Second last channel should be size 3, got {preds.shape[-3]}")
 
         # Calculate loss for regression and classification and then calculate weighted sum
         regression_loss = self.regression_loss_1(preds[:, :1].contiguous(), targets.contiguous()) + self.regression_loss_2(preds[:, :1].contiguous(), targets.contiguous())
         regression_loss = regression_loss / 2
 
-        classification_loss = self.classification_loss_1(preds[:, 1:], targets.long()) + self.classification_loss_2(preds[:, 1:], targets.long())
+        classification_loss = self.classification_loss_1(preds[:, 1:].contiguous(), targets.long()) + self.classification_loss_2(preds[:, 1:].contiguous(), targets.long())
         classification_loss = classification_loss / 2
 
         return (regression_loss + classification_loss * self.classification_weight) / (1 + self.classification_weight)
