e54b35f23f581d7588685c2f453ef01c5f21de05
tolgakopar <cahittolgakopar@gmail.com>
2024-02-02 15:36:19 +0100

    added an extra param to padded model for torchgeo
    You can now give torchgeo weights to padded model and it will overwrite the weights of the encoder with torchgeo ones. It will also duplicate the weigths along different channels if the num channels is larger than the pretrained channels.

--- src/pipeline/model/architectures/padded_model.py
+++ src/pipeline/model/architectures/padded_model.py
@@ -1,7 +1,7 @@
 """Padded model architecture, suitable for instantiation for pytorch built-in models."""
 import torch
 from torch import nn
-
+from typing import Any
 
 class PaddedModel(nn.Module):
     """Model architecture with padding.This class is used to wrap a pytorch model and add padding to the input image if necessary.
@@ -11,7 +11,7 @@ class PaddedModel(nn.Module):
     :param activation: activation function to be applied to the output of the model
     """
 
-    def __init__(self, model: nn.Module, padding: int = 0, activation: None | nn.Module = None) -> None:
+    def __init__(self, model: nn.Module, padding: int = 0, activation: None | nn.Module = None, weights: Any = None) -> None:
         """Initialize the PaddedModel.
 
         :param model: Pytorch model to be used
@@ -22,6 +22,10 @@ class PaddedModel(nn.Module):
         self.model = model
         self.padding = padding
         self.activation = activation
+        if weights is not None:
+            state_dict = weights.get_state_dict(progress=True)
+            new_state_dict = self.expand_model_channels(state_dict, default_in_channels=7, new_in_channels=11)
+            model.encoder.load_state_dict(new_state_dict)
         # Create a padding layer to pad the input image to a suitable size
         if padding > 0:
             self.padding_layer = nn.ZeroPad2d((padding, padding, padding, padding))
@@ -46,3 +50,15 @@ class PaddedModel(nn.Module):
             y = self.activation(y)
 
         return y
+
+    def expand_model_channels(self, state_dict, default_in_channels=3, new_in_channels=7):
+        for key in list(state_dict.keys()):
+            if 'conv' in key and 'weight' in key:
+                weight = state_dict[key]
+                if weight.size(1) == default_in_channels:
+                    new_weight = torch.Tensor(weight.size(0), new_in_channels, *weight.size()[2:])
+                    for i in range(new_in_channels):
+                        new_weight[:, i] = weight[:, i % default_in_channels]
+                    new_weight = new_weight * (default_in_channels / new_in_channels)
+                    state_dict[key] = new_weight
+        return state_dict
